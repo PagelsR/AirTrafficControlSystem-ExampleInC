04424708eca86240979d794cafc625d7520f9998
Randy Pagels <pagelsr@live.com>
2023-12-27 14:41:23 -0500

    Refactor airplane control logic and add unit tests

--- AirTraffic.py
+++ AirTraffic.py
@@ -23,20 +23,19 @@ class Airplane:
         print(f"[AC]: Flight {self.id} requesting landing")
         runway_sem.acquire()
         stop = False
-        while not stop:
-            for i in range(NUMBER_OF_RUNWAYS):
-                if runway_array[i] == 0 and (taxiway_array[i][0] == 0 or taxiway_array[i][1] == 0 or taxiway_array[i][2] == 0) and queue_sem[i].acquire(blocking=False):
-                    runway_array[i] = self.id
-                    stop = True
-                    break
+        for i in range(NUMBER_OF_RUNWAYS):
+            if runway_array[i] == 0 and (taxiway_array[i][0] == 0 or taxiway_array[i][1] == 0 or taxiway_array[i][2] == 0) and queue_sem[i].acquire(blocking=False):
+                runway_array[i] = self.id
+                stop = True
+                break
         print(f"[GC]: Airplane {self.id} assigned runway {i + 1}")
         print(f"[AC]: Airplane {self.id} has landed")
 
     def proceed_to_taxiways(self):
-        runway = runway_array.index(self.id)
-        for i in range(3):
-            if taxiway_array[runway][i] == 0:
-                taxiway_array[runway][i] = self.id
+        runway = next(i for i in range(NUMBER_OF_RUNWAYS) if runway_array[i] == self.id)
+        for j in range(3):
+            if taxiway_array[runway][j] == 0:
+                taxiway_array[runway][j] = self.id
                 runway_array[runway] = 0
                 runway_sem.release()
                 break
@@ -45,17 +44,26 @@ class Airplane:
     def proceed_to_gate(self):
         print(f"[AC]: Airplane {self.id} requesting gate")
         gate_sem.acquire()
-        runway = next(i for i, row in enumerate(taxiway_array) if self.id in row)
-        taxiway_array[runway][taxiway_array[runway].index(self.id)] = 0
-        queue_sem[runway].release()
-        gate_array[gate_array.index(0)] = self.id
-        print(f"[GC]: Airplane {self.id} assigned gate {gate_array.index(self.id) + 1}")
+        for i in range(NUMBER_OF_RUNWAYS):
+            for j in range(3):
+                if taxiway_array[i][j] == self.id:
+                    queue_sem[i].release()
+                    taxiway_array[i][j] = 0
+                    break
+        for i in range(NUMBER_OF_GATES):
+            if gate_array[i] == 0:
+                gate_array[i] = self.id
+                break
+        print(f"[GC]: Airplane {self.id} assigned gate {i + 1}")
 
     def proceed_to_hangar(self):
         time.sleep(5)
         print(f"[AC]: Airplane {self.id} heading to hangar")
-        gate_array[gate_array.index(self.id)] = 0
-        gate_sem.release()
+        for i in range(NUMBER_OF_GATES):
+            if gate_array[i] == self.id:
+                gate_array[i] = 0
+                gate_sem.release()
+                break
 
     def airplane_control(self):
         self.landing_function()
@@ -63,18 +71,12 @@ class Airplane:
         self.proceed_to_gate()
         self.proceed_to_hangar()
 
-    def start(self):
-        self.thread.start()
-
-    def join(self):
-        self.thread.join()
-
 def main():
     airplanes = [Airplane(i+1) for i in range(NUMBER_OF_AIRPLANES)]
     for airplane in airplanes:
-        airplane.start()
+        airplane.thread.start()
     for airplane in airplanes:
-        airplane.join()
+        airplane.thread.join()
 
 if __name__ == "__main__":
     main()
--- test_AirTraffic.py
+++ test_AirTraffic.py
@@ -1,62 +1,41 @@
 import unittest
 from unittest.mock import patch
+from io import StringIO
+
 from AirTraffic import Airplane
 
-class AirplaneTest(unittest.TestCase):
+# Test case for the Airplane class
+class AirplaneTestCase(unittest.TestCase):
+    def setUp(self):
+        self.airplane = Airplane(1)
+
+    # Test the landing_function method
     def test_landing_function(self):
-        """
-        Test case for the landing_function method of the Airplane class.
+        expected_output = "[AC]: Flight 1 requesting landing\n[GC]: Airplane 1 assigned runway 1\n[AC]: Airplane 1 has landed\n"
+        with patch('sys.stdout', new=StringIO()) as fake_output:
+            self.airplane.landing_function()
+            self.assertEqual(fake_output.getvalue(), expected_output)
 
-        This test case verifies that the landing_function method correctly prints a message when an airplane lands.
-        """
-        airplane = Airplane(1)
-        with patch('builtins.print') as mock_print:
-            airplane.landing_function()
-            mock_print.assert_called_with("[AC]: Airplane 1 has landed")
-
+    # Test the proceed_to_taxiways method
     def test_proceed_to_taxiways(self):
-        """
-        Test case for the proceed_to_taxiways method of the Airplane class.
+        expected_output = "[AC]: Airplane 1 queuing in taxiway of runway 1\n"
+        with patch('sys.stdout', new=StringIO()) as fake_output:
+            self.airplane.proceed_to_taxiways()
+            self.assertEqual(fake_output.getvalue(), expected_output)
 
-        This test case verifies that the proceed_to_taxiways method correctly prints a message when an airplane queues in a taxiway.
-        """
-        airplane = Airplane(1)
-        airplane.id = 1
-        airplane.runway_array = [1, 0, 0]
-        airplane.taxiway_array = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-        airplane.runway_sem = threading.Semaphore()
-        with patch('builtins.print') as mock_print:
-            airplane.proceed_to_taxiways()
-            mock_print.assert_called_with("[AC]: Airplane 1 queuing in taxiway of runway 1")
-
+    # Test the proceed_to_gate method
     def test_proceed_to_gate(self):
-        """
-        Test case for the proceed_to_gate method of the Airplane class.
+        expected_output = "[AC]: Airplane 1 requesting gate\n[GC]: Airplane 1 assigned gate 1\n"
+        with patch('sys.stdout', new=StringIO()) as fake_output:
+            self.airplane.proceed_to_gate()
+            self.assertEqual(fake_output.getvalue(), expected_output)
 
-        This test case verifies that the proceed_to_gate method correctly prints a message when an airplane is assigned a gate.
-        """
-        airplane = Airplane(1)
-        airplane.id = 1
-        airplane.taxiway_array = [[0, 0, 0], [0, 0, 0], [1, 0, 0]]
-        airplane.gate_array = [0, 0, 0]
-        airplane.gate_sem = threading.Semaphore()
-        with patch('builtins.print') as mock_print:
-            airplane.proceed_to_gate()
-            mock_print.assert_called_with("[GC]: Airplane 1 assigned gate 1")
-
+    # Test the proceed_to_hangar method
     def test_proceed_to_hangar(self):
-        """
-        Test case for the proceed_to_hangar method of the Airplane class.
-
-        This test case verifies that the proceed_to_hangar method correctly prints a message when an airplane heads to the hangar.
-        """
-        airplane = Airplane(1)
-        airplane.id = 1
-        airplane.gate_array = [1, 0, 0]
-        airplane.gate_sem = threading.Semaphore()
-        with patch('builtins.print') as mock_print:
-            airplane.proceed_to_hangar()
-            mock_print.assert_called_with("[AC]: Airplane 1 heading to hangar")
+        expected_output = "[AC]: Airplane 1 heading to hangar\n"
+        with patch('sys.stdout', new=StringIO()) as fake_output:
+            self.airplane.proceed_to_hangar()
+            self.assertEqual(fake_output.getvalue(), expected_output)
 
 if __name__ == '__main__':
     unittest.main()
